3b2520028d43fe8731f0d3b83441148f49828243
Ashley <ashleyq007@gmail.com>
2025-07-31 19:53:52 -0700

    Update main.js

--- main.js
+++ main.js
@@ -14,3 +14,29 @@ document.addEventListener("DOMContentLoaded", () => {
   }, 4000);
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+  const salaryMapDiv = document.getElementById("map");
+  const layoffMapDiv = document.getElementById("layoff-map");
+  const mapTitle = document.getElementById("map-view-title");
+
+  const salaryBtn = document.getElementById("show-salary-map");
+  const layoffBtn = document.getElementById("show-layoff-map");
+
+  salaryBtn.addEventListener("click", () => {
+    salaryMapDiv.style.display = "block";
+    layoffMapDiv.style.display = "none";
+    mapTitle.textContent = "U.S. Salary Map";
+
+    salaryBtn.classList.add("active");
+    layoffBtn.classList.remove("active");
+  });
+
+  layoffBtn.addEventListener("click", () => {
+    salaryMapDiv.style.display = "none";
+    layoffMapDiv.style.display = "block";
+    mapTitle.textContent = "U.S. Layoff Map";
+
+    layoffBtn.classList.add("active");
+    salaryBtn.classList.remove("active");
+  });
+});
